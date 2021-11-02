f1eb412018b61f91e91e83d2f7c7013e86d8e082
dzx-dzx <31068367+dzx-dzx@users.noreply.github.com>
2021-11-02 19:21:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { fetchData as fetchDataBath } from './bath.js';
 import { fetchData as fetchDataStudy } from './study.js';
+import { fetchData as fetchDataCainiao } from './cainiao.js';
 
 fetchDataBath().then()
 fetchDataStudy().then()
+fetchDataCainiao().then()
 
 setInterval(fetchDataBath, 15000);
 setInterval(fetchDataStudy, 15000);
+setInterval(fetchDataCainiao, 15000);
