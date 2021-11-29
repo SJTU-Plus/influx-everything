1f88d8095dc35dcf997ff4661fb3586d698a4575
dzx-dzx <31068367+dzx-dzx@users.noreply.github.com>
2021-11-29 19:27:22 +0800

    Migrate to another API

--- study.js
+++ study.js
@@ -10,28 +10,19 @@ writeApi.useDefaultTags({ host: 'host1' })
 const knownBuildIDs = [{ buildId: 564, buildingName: "东中院" }, { buildId: 128, buildingName: "中院" }]//Can be obtained from /build/findAreaBuild
 
 export async function fetchData() {
-    for (const { buildId, buildingName } of knownBuildIDs) {
-        const buildings = await axios.post('https://ids.sjtu.edu.cn/build/findBuildRoomType', `buildId=${buildId}&mobileType=mobileFlag`, { headers: { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' } })
+
+    const rooms = await axios.post('https://ids.sjtu.edu.cn/classRoom/getByFreeClassroomInfo', `roomCode=LGXQ`, { headers: { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' } })
             .then(function (response) {
-                return response.data.data.floorList
-            })
-            .catch(err => console.error(err));
-        const timeSection = await axios.post('https://ids.sjtu.edu.cn/course/findSection', {})
-            .then(function (response) {
-                return response.data.data.curSection
-            })
-            .catch(err => console.error(err));
-        for (const building of buildings) 
-            for (const room of building.children)
-                if (room["freeRoom"] == '1' && ((room["roomCourseList"]?.every((course) => course.startSection > timeSection || course.endSection < timeSection)) ?? true)){
+                return JSON.parse(response.data.data.freeClassRoomList)
+            }).catch(err => console.error(err));
+    for (const room of rooms)
+    {
                     const roomPoint = new Point('study_room')
-                         .tag("room_name", room["text"])
-                         .tag("building_name", buildingName)
-                         .intField("used",room["actualStuNum"] ?? 0)//网站上就这么显示,不服打教务处(电话)
-                         .intField("all", Number(room["zws"]))
+                         .tag("room_name", room["name"])
+                         .tag("building_name", room["buildName"])
+                         .intField("used",room["realTimeNum"])
+                         .intField("all", Number(room["kwNum"]))
                     writeApi.writePoint(roomPoint)
-                }
-        
     }
     await writeApi.flush()
 }
